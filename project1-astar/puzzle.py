41904302e947deca2dea3363e661a1b42d07979a
domnu-raul <sir.c4ppuccin0@gmail.com>
2024-10-04 12:37:27 +0300

    init, testing done

---
+++ project1-astar/puzzle.py
@@ -0,0 +1,71 @@
+from enum import Enum
+from typing import List, Optional
+
+
+class Vector2:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+    def __sub__(self, other):
+        return Vector2(self.x - other.x, self.y - other.y)
+
+    def __add__(self, other):
+        return Vector2(self.x + other.x, self.y + other.y)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+    def __repr__(self):
+        return str(self)
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+
+class Direction(Enum):
+    UP = Vector2(0, -1)
+    DOWN = Vector2(0, 1)
+    LEFT = Vector2(-1, 0)
+    RIGHT = Vector2(1, 0)
+
+
+class Puzzle:
+    def __init__(self, grid: Optional[List[List[int]]] = None):
+        if grid:
+            self._grid = grid
+        else:
+            self._grid = [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8]
+            ]
+
+    @property
+    def grid(self):
+        return self._grid
+
+    def get_square(self, position: Vector2):
+        return self._grid[position.y][position.x]
+
+    def move(self, position: Vector2, direction: Direction):
+        if position.x < 0 or position.x >= 3 or position.y < 0 or position.y >= 3:
+            return
+
+        square = self.get_square(position)
+        if square == 0:
+            return
+
+        other_position = position + direction.value
+
+        if other_position.x < 0 or other_position.x >= 3 or other_position.y < 0 or other_position.y >= 3:
+            return
+
+        other_square = self.get_square(other_position)
+
+        if other_square == 0:
+            self._grid[other_position.y][other_position.x] = square
+            self._grid[position.y][position.x] = 0
---
+++ tests/test_puzzle.py
@@ -0,0 +1,168 @@
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
+from puzzle import Puzzle, Vector2, Direction
+
+
+def test_puzzle():
+    puzzle = Puzzle()
+    assert puzzle is not None
+
+
+def test_puzzle_2():
+    puzzle = Puzzle()
+    expected_grid = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]
+    ]
+
+    assert puzzle.grid == expected_grid
+
+
+def test_get_square():
+    puzzle = Puzzle()
+    square = puzzle.get_square(Vector2(0, 0))
+    assert square == 0
+
+
+def test_get_square_2():
+    puzzle = Puzzle()
+    square = puzzle.get_square(Vector2(1, 1))
+    assert square == 4
+
+
+def test_move_up():
+    puzzle = Puzzle([
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]
+    ])
+
+    puzzle.move(Vector2(0, 1), Direction.UP)
+    expected_grid = [
+        [3, 1, 2],
+        [0, 4, 5],
+        [6, 7, 8]
+    ]
+
+    assert puzzle.grid == expected_grid
+
+def test_move_down():
+    puzzle = Puzzle([
+        [3, 1, 2],
+        [0, 4, 5],
+        [6, 7, 8]
+    ])
+
+    puzzle.move(Vector2(0, 0), Direction.DOWN)
+
+    expected_grid = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]
+    ]
+
+    assert puzzle.grid == expected_grid
+
+def test_move_left():
+    puzzle = Puzzle([
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]
+    ])
+
+    puzzle.move(Vector2(1, 0), Direction.LEFT)
+
+    expected_grid = [
+        [1, 0, 2],
+        [3, 4, 5],
+        [6, 7, 8]
+    ]
+
+    assert puzzle.grid == expected_grid
+
+def test_move_right():
+    puzzle = Puzzle([
+        [1, 0, 2],
+        [3, 4, 5],
+        [6, 7, 8]
+    ])
+
+    puzzle.move(Vector2(0, 0), Direction.RIGHT)
+
+    expected_grid = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]
+    ]
+
+    assert puzzle.grid == expected_grid
+
+def test_move_up_2():
+    puzzle = Puzzle([
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]
+    ])
+
+    puzzle.move(Vector2(0, 2), Direction.UP)
+
+    expected_grid = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]
+    ]
+
+    assert puzzle.grid == expected_grid
+
+def test_move_down_2():
+    puzzle = Puzzle([
+        [3, 1, 2],
+        [6, 4, 5],
+        [0, 7, 8]
+    ])
+
+    puzzle.move(Vector2(0, 0), Direction.DOWN)
+
+    expected_grid = [
+        [3, 1, 2],
+        [6, 4, 5],
+        [0, 7, 8]
+    ]
+
+    assert puzzle.grid == expected_grid
+
+def test_move_left_2():
+    puzzle = Puzzle([
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]
+    ])
+
+    puzzle.move(Vector2(2, 0), Direction.LEFT)
+
+    expected_grid = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]
+    ]
+
+    assert puzzle.grid == expected_grid
+
+def test_move_right_2():
+    puzzle = Puzzle([
+        [1, 2, 0],
+        [3, 4, 5],
+        [6, 7, 8]
+    ])
+
+    puzzle.move(Vector2(0, 0), Direction.RIGHT)
+
+    expected_grid = [
+        [1, 2, 0],
+        [3, 4, 5],
+        [6, 7, 8]
+    ]
+
+    assert puzzle.grid == expected_grid
