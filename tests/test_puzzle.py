fcc4a61530f8cfd59a1c695981a96be922c4a23c
domnu-raul <sir.c4ppuccin0@gmail.com>
2024-10-04 12:48:31 +0300

    fixed imports

--- tests/test_puzzle.py
+++ tests/test_puzzle.py
@@ -1,7 +1,6 @@
 import os
 import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
-from puzzle import Puzzle, Vector2, Direction
+from src.puzzle import Puzzle, Vector2, Direction
 
 
 def test_puzzle():
